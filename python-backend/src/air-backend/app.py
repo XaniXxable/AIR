8deaab7f4e18f5f1ff0f1b238b5decf7a92e43e8
Matthias Hemmer <xanixxable@gmail.com>
2024-11-25 15:10:51 +0100

    Run backend inside docker

--- python-backend/src/air-backend/app.py
+++ python-backend/src/air-backend/app.py
@@ -1,2 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
+from typing import Any
 
+app = FastAPI()
+
+@app.get("/")
+def index() -> Any:
+  return {"Hello": "World"}
+
+
+def start() -> None:
+  """Launched with `poetry run start` at root level"""
+  uvicorn.run("air-backend.app:app", host="0.0.0.0", port=8000, reload=True)
