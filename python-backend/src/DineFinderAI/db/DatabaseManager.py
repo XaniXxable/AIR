5e77295009cf233d8278f7a99ef801f66bb42e29
Matthias Hemmer <xanixxable@gmail.com>
2024-12-16 11:34:30 +0100

    Use pandas for data storage

--- python-backend/src/DineFinderAI/db/DatabaseManager.py
+++ python-backend/src/DineFinderAI/db/DatabaseManager.py
@@ -5,7 +5,7 @@ It is used to provide the testdata for the ML model.
 
 import sqlite3
 import pathlib
-import pandas
+import pandas as pd
 from typing import Tuple, Any
 
 
@@ -20,18 +20,11 @@ class DatabaseManager:
   def closeFunc(self) -> None:
     self.con.close()
 
-  def execute(self, stmt: str, params: Tuple[Any, ...] | list[any] = ()) -> list[any]:
-    try:
-      cursor = self.con.cursor()
-      cursor.execute(stmt, params)
-      ret = cursor.fetchall()
-      self.con.commit()
-      return ret
-    finally:
-      cursor.close()
+  def execute(self, query: str) -> pd.DataFrame:
+    return pd.read_sql_query(query, self.con)
 
   def insertData(self) -> None:
-    db = pandas.read_json(self.json_filepath, lines=True)
+    db = pd.read_json(self.json_filepath, lines=True)
     db = db[["name", "address", "city", "state", "postal_code", "stars", "review_count", "categories"]]
     db = db[db["categories"].str.contains("Restaurants") == True]
     db.to_sql("restaurants", self.con)
