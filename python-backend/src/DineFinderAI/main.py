c5702a4cbee745e56228d46c12822adeaa98efd7
Matthias Hemmer <xanixxable@gmail.com>
2024-12-29 16:45:23 +0100

    Pre trained model added

--- python-backend/src/DineFinderAI/main.py
+++ python-backend/src/DineFinderAI/main.py
@@ -1,5 +1,17 @@
 from interface.response.queryResponse import QueryResponse
 from interface.request.queryRequest import QueryRequest
+from DineFinderAI.models.DineFinder import DineFinder
 
-def training(query: QueryRequest) -> QueryResponse:
-  print(query)
+model = DineFinder()
+
+
+def training() -> None:
+  request = QueryRequest(None, "Where can I find good bubble tea in philadelphia?")
+
+  data = model.execute(request)
+  for restaurant in data:
+    print(f"Name: {restaurant['name']}, Score: {restaurant['score']:.2f}")
+    print(f"Address: {restaurant['address']}")
+    print(f"Categories: {restaurant['categories']}")
+    print(f"Stars: {restaurant['stars']}, Reviews: {restaurant['review_count']}")
+    print("-" * 50)
