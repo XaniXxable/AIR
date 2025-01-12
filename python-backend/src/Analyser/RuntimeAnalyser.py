df597b2cf732cfe95de63178751fd0a86f3bfe65
Matthias Hemmer <xanixxable@gmail.com>
2025-01-12 10:44:05 +0100

    Added docstrings

--- python-backend/src/Analyser/RuntimeAnalyser.py
+++ python-backend/src/Analyser/RuntimeAnalyser.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+
 class RuntimeAnalyser:
   """
   A class for analyzing runtime data and storing findings.
   """
+
   def __init__(self, findings_folder_path: Path) -> None:
     """
     Initializes the RuntimeAnalyser with a folder path for the findings.
@@ -26,7 +28,7 @@ class RuntimeAnalyser:
     if not found_cities_path.is_file() or not found_categories_path.is_file():
       print("No files found for runtime analysis. Please run 'poetry run analyse_queries' to get the required files...")
       return
-    
+
     self._analyse_runtime_of_cities_queries(found_cities_path)
     self._analyse_runtime_of_categories_queries(found_categories_path)
 
@@ -42,13 +44,13 @@ class RuntimeAnalyser:
     mean_runtime = np.mean(runtimes)
     std_runtime = np.std(runtimes)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14,7))
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14, 7))
     ax.plot(runtimes)
-    ax.set_title(f'Runtimes for processing a city query (μ={mean_runtime:.2f} ms, σ={std_runtime:.2f} ms)', fontsize=18)
-    ax.set_xlabel('Query number', fontsize=14)
-    ax.set_ylabel('Duration [ms]', fontsize=14)
-    ax.tick_params(axis='x', which='major', labelsize=11)
-    ax.tick_params(axis='y', which='major', labelsize=11)
+    ax.set_title(f"Runtimes for processing a city query (μ={mean_runtime:.2f} ms, σ={std_runtime:.2f} ms)", fontsize=18)
+    ax.set_xlabel("Query number", fontsize=14)
+    ax.set_ylabel("Duration [ms]", fontsize=14)
+    ax.tick_params(axis="x", which="major", labelsize=11)
+    ax.tick_params(axis="y", which="major", labelsize=11)
     ax.grid(True)
 
     output_path = self.findings_folder_path.joinpath("city_queries_runtime.png")
@@ -68,13 +70,15 @@ class RuntimeAnalyser:
     mean_runtime = np.mean(runtimes)
     std_runtime = np.std(runtimes)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14,7))
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14, 7))
     ax.plot(runtimes)
-    ax.set_title(f'Runtimes for processing a category query (μ={mean_runtime:.2f} ms, σ={std_runtime:.2f} ms)', fontsize=18)
-    ax.set_xlabel('Query number', fontsize=14)
-    ax.set_ylabel('Duration [ms]', fontsize=14)
-    ax.tick_params(axis='x', which='major', labelsize=11)
-    ax.tick_params(axis='y', which='major', labelsize=11)
+    ax.set_title(
+      f"Runtimes for processing a category query (μ={mean_runtime:.2f} ms, σ={std_runtime:.2f} ms)", fontsize=18
+    )
+    ax.set_xlabel("Query number", fontsize=14)
+    ax.set_ylabel("Duration [ms]", fontsize=14)
+    ax.tick_params(axis="x", which="major", labelsize=11)
+    ax.tick_params(axis="y", which="major", labelsize=11)
     ax.grid(True)
 
     output_path = self.findings_folder_path.joinpath("category_queries_runtime.png")
@@ -92,6 +96,7 @@ def main() -> None:
   runtime_analyser = RuntimeAnalyser(findings_folder_path)
 
   runtime_analyser()
+
 
 if __name__ == "__main__":
   import sys
--- python-backend/src/Analyser/TrainingAnalyser.py
+++ python-backend/src/Analyser/TrainingAnalyser.py
@@ -4,37 +4,47 @@ import pandas as pd
 
 
 class TrainingAnalyser:
-    def __init__(self, logging_dir: Path) -> None:
-        self.data = pd.read_json(logging_dir)
-        self.data.dropna(subset=['eval_loss'], inplace=True)
+  """
+  A class for analyzing eval loss data and storing findings.
+  """
 
-    def plot(self, output_path: Path) -> None:
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14,7))
-        ax.plot(self.data["epoch"], self.data["eval_loss"], label="Eval Loss")
-        ax.set_title("Evaluation Loss Over Epochs", fontsize=18)
-        ax.set_xlabel("Epoch", fontsize=14)
-        ax.set_ylabel("Evaluation Loss", fontsize=14)
-        ax.tick_params(axis='x', which='major', labelsize=11)
-        ax.tick_params(axis='y', which='major', labelsize=11)
-        ax.legend()
-        
-        file_format = output_path.name.split(".")[-1]
-        fig.savefig(fname=str(output_path), format=file_format)
-        print(f"Saved {output_path.resolve()}")
+  def __init__(self, logging_dir: Path) -> None:
+    self.data = pd.read_json(logging_dir)
+    self.data.dropna(subset=["eval_loss"], inplace=True)
+
+  def plot(self, output_path: Path) -> None:
+    """
+    Analyzes the eval loss of training and generates a plot.
+
+    Args:
+        output_path (Path): Path to the JSON file containing the training metrics.
+    """
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14, 7))
+    ax.plot(self.data["epoch"], self.data["eval_loss"], label="Eval Loss")
+    ax.set_title("Evaluation Loss Over Epochs", fontsize=18)
+    ax.set_xlabel("Epoch", fontsize=14)
+    ax.set_ylabel("Evaluation Loss", fontsize=14)
+    ax.tick_params(axis="x", which="major", labelsize=11)
+    ax.tick_params(axis="y", which="major", labelsize=11)
+    ax.legend()
+
+    file_format = output_path.name.split(".")[-1]
+    fig.savefig(fname=str(output_path), format=file_format)
+    print(f"Saved {output_path.resolve()}")
 
 
 def main() -> None:
-    """
-    Entry point of the script.
-    """
-    logging_path = Path.cwd().joinpath("resources", "model", "training", "fold_0", "training_logs.json")
-    output_path = Path.cwd().joinpath("resources", "findings", "eval_loss.png")
-    runtime_analyser = TrainingAnalyser(logging_path)
+  """
+  Entry point of the script.
+  """
+  logging_path = Path.cwd().joinpath("resources", "model", "training", "fold_0", "training_logs.json")
+  output_path = Path.cwd().joinpath("resources", "findings", "eval_loss.png")
+  runtime_analyser = TrainingAnalyser(logging_path)
 
-    runtime_analyser.plot(output_path)
+  runtime_analyser.plot(output_path)
 
 
 if __name__ == "__main__":
-    import sys
+  import sys
 
-    sys.exit(main())
+  sys.exit(main())
